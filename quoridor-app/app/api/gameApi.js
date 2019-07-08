6461eec0e33a52601b3dd9dc971cc3c41337026e
Matthieu Chaffotte <matthieu.chaffotte@gmail.com>
2019-07-08 17:44:59 +0200

    use constants

--- quoridor-app/app/api/gameApi.js
+++ quoridor-app/app/api/gameApi.js
@@ -1,6 +1,9 @@
 const API_PATH = "http://192.168.86.56:8383/games";
 const JSON_TYPE = "application/json";
 
+const DEFAULT_BOARD_SIZE = 9;
+const DEFAULT_NUMBER_OF_FENCES_PER_PLAYER = 10;
+
 export const createGame = async () => {
   const response = await fetch(API_PATH, {
     method: "POST",
@@ -9,8 +12,8 @@ export const createGame = async () => {
       "Content-Type": JSON_TYPE
     },
     body: JSON.stringify({
-      boardSize: 9,
-      numberOfFencesPerPlayer: 10
+      boardSize: DEFAULT_BOARD_SIZE,
+      numberOfFencesPerPlayer: DEFAULT_NUMBER_OF_FENCES_PER_PLAYER
     })
   });
   return await response.json();
