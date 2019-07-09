53b3b151ff15d32c2f0b398a69db6924b41daed3
Matthieu Chaffotte <matthieu.chaffotte@gmail.com>
2019-07-09 15:13:22 +0200

    add test

---
+++ quoridor-app/app/component/__tests__/board-test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Board from "../board";
+import Pawn from "../pawn";
+import Square from "../square";
+
+describe("Board Componenet tests", () => {
+  it("should render with two squares and one pawn both well positioned", () => {
+    const testRenderer = renderer.create(
+      <Board
+        squares={[{ column: 10, row: 8 }, { column: 10, row: 5 }]}
+        pawns={[{ goal: 2, position: { column: 5, row: 2 } }]}
+      />
+    );
+    const testInstance = testRenderer.root;
+
+    const pawnComponent = testInstance.findByType(Pawn);
+    expect(pawnComponent).toBeDefined();
+    expect(pawnComponent.props.position).toStrictEqual({
+      top: 79,
+      left: 209
+    });
+
+    const squareComponents = testInstance.findAllByType(Square);
+    expect(squareComponents.length).toBe(2);
+    expect(squareComponents[0].props.position).toStrictEqual({
+      top: 325,
+      left: 405
+    });
+    expect(squareComponents[1].props.position).toStrictEqual({
+      top: 205,
+      left: 405
+    });
+  });
+});
